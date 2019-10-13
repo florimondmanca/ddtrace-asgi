8d876f384fced4f75566669b77089d5a9f20c81d
florimondmanca <florimond.manca@gmail.com>
2019-10-13 20:18:56 +0200

    Initial commit

---
+++ ddtrace_asgi/middleware.py
@@ -0,0 +1,86 @@
+import sys
+import typing
+
+from ddtrace.compat import iteritems
+from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
+from ddtrace.ext import http as http_tags
+from ddtrace.http import store_request_headers, store_response_headers
+from ddtrace.propagation.http import HTTPPropagator
+from ddtrace.settings import config
+from ddtrace.tracer import Tracer
+from ddtrace.span import Span
+
+from starlette.datastructures import Headers, URL
+from starlette.requests import Request
+from starlette.types import ASGIApp, Receive, Scope, Send, Message
+
+
+class TraceMiddleware:
+    def __init__(
+        self,
+        app: ASGIApp,
+        tracer: Tracer,
+        service: str = "asgi",
+        distributed_tracing: bool = True,
+    ) -> None:
+        self.app = app
+        self.tracer = tracer
+        self.service = service
+        self._distributed_tracing = distributed_tracing
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+
+        request = Request(scope=scope, receive=receive)
+
+        if self._distributed_tracing:
+            propagator = HTTPPropagator()
+            context = propagator.extract(request.headers)
+            if context.trace_id:
+                self.tracer.context_provider.activate(context)
+
+        resource = "%s %s" % (request.method, request.url.path)
+        span = self.tracer.trace(
+            name="asgi.request",
+            service=self.service,
+            resource=resource,
+            span_type=http_tags.TYPE,
+        )
+
+        span.set_tag(
+            ANALYTICS_SAMPLE_RATE_KEY,
+            config.asgi.get_analytics_sample_rate(use_global_config=True),
+        )
+        span.set_tag(http_tags.METHOD, request.method)
+        span.set_tag(http_tags.URL, str(request.url))
+        if config.asgi.trace_query_string:
+            span.set_tag(http_tags.QUERY_STRING, request.url.query)
+
+        # NOTE: any request header set in the future will not be stored in the span.
+        store_request_headers(request.headers, span, config.asgi)
+
+        async def send_with_tracing(message: Message) -> None:
+            span = self.tracer.current_span()
+
+            if not span:
+                # Unexpected.
+                await send(message)
+                return
+
+            if message["type"] == "http.response.start":
+                status_code: int = message["status"]
+                response_headers = Headers(raw=message["headers"])
+                store_response_headers(response_headers, span, config.asgi)
+                span.set_tag(http_tags.STATUS_CODE, str(status_code))
+
+            await send(message)
+
+        try:
+            await self.app(scope, receive, send_with_tracing)
+        except BaseException as exc:
+            span.set_traceback()
+            raise exc from None
+        finally:
+            span.finish()
---
+++ tests/test_integration.py
@@ -0,0 +1,56 @@
+import typing
+
+import httpx
+import pytest
+from ddtrace import config
+from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
+from ddtrace.ext import errors as err_ext, http as http_ext
+from ddtrace.tracer import Tracer
+from starlette.types import Receive, Scope, Send
+from ddtrace.span import Span
+
+from ddtrace_asgi.middleware import TraceMiddleware
+
+
+async def hello_world(scope: Scope, receive: Receive, send: Send) -> None:
+    assert scope["type"] == "http"
+    await send(
+        {
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [[b"content-type", b"text/plain"]],
+        }
+    )
+    await send({"type": "http.response.body", "body": b"Hello, world!"})
+
+
+@pytest.fixture
+def tracer() -> Tracer:
+    from .utils.tracer import DummyTracer
+
+    return DummyTracer()
+
+
+@pytest.fixture
+def client(tracer: Tracer) -> typing.Iterator[httpx.Client]:
+    app = TraceMiddleware(hello_world, tracer, service="test_app")
+    with httpx.Client(app=app, base_url="http://testserver") as client:
+        yield client
+
+
+def test_app(client: httpx.Client, tracer: Tracer):
+    r = client.get("/example")
+    assert r.status_code == 200
+    assert r.text == "Hello, world!"
+
+    traces = tracer.writer.pop_traces()
+    assert len(traces) == 1
+    assert len(traces[0]) == 1
+    span: Span = traces[0][0]
+    assert span.name == "asgi.request"
+    assert span.service == "test_app"
+    assert span.resource == "GET /example"
+    assert span.get_tag(http_ext.STATUS_CODE) == "200"
+    assert span.get_tag(http_ext.URL) == "http://testserver/example"
+    assert http_ext.QUERY_STRING not in span.meta
+    assert span.parent_id is None
