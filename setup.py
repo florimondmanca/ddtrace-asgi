48eac98a209831ef028db640b52e0802b3278ad6
florimondmanca <florimond.manca@gmail.com>
2019-10-23 21:06:22 +0200

    Fix bad version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ddtrace-asgi",
-    version="0.1",
+    version="0.0.1",
     description="Unofficial ddtrace integration for ASGI apps and frameworks",
     url="http://github.com/florimondmanca/ddtrace-asgi",
     author="Florimond Manca",
