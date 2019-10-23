2016d79984f915d6cb59e9972f6c93047352897c
florimondmanca <florimond.manca@gmail.com>
2019-10-23 20:26:05 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ddtrace-asgi",
+    version="0.1",
+    description="Unofficial Datadog Tracing integration for ASGI apps and frameworks",
+    url="http://github.com/florimondmanca/ddtrace-asgi",
+    author="Florimond Manca",
+    author_email="florimond.manca@gmail.com",
+    license="MIT",
+    packages=["ddtrace_asgi"],
+    zip_safe=False,
+)
